9d874bd5d6660e82f12c4c7743f94e88e55c9cba
Cody Usher <usher.cody@outlook.com>
2021-02-13 08:13:17 -0700

    starting for the day

--- src/pages/ListPage.js
+++ src/pages/ListPage.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
 
-export default function Contact(props) {
+export default function ListPage(props) {
 	return <div className="ContactPage">This is the {props.page} page</div>;
 }
