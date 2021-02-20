b8a7929567768f9031b0f2e6558c729999ac8eee
Cody Usher <usher.cody@outlook.com>
2021-02-20 08:28:49 -0700

    start of class

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,11 +4,13 @@ import { Link } from 'react-router-dom';
 const NavBar = props => {
 	return (
 		<nav className="NavBar">
-			{props.routes.map(({ key, path }) => (
-				<Link key={key} to={path}>
-					{key}
-				</Link>
-			))}
+			{props.routes
+				.filter(item => !item.path.includes(':'))
+				.map(({ key, path }) => (
+					<Link key={key} to={path}>
+						{key}
+					</Link>
+				))}
 		</nav>
 	);
 };
